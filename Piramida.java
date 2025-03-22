ec3fe3e87afabb3a3aeaaf45ef2575cd7227bf96
Denis-G25 <gabordenis1@gmail.com>
2025-03-22 21:54:30 +0200

    Add files via upload

---
+++ Piramida.java
@@ -0,0 +1,27 @@
+
+public class Piramida {
+    private String n;
+
+   
+    public Piramida(String n) {
+        this.n = n;
+    }
+
+    
+    public void tiparestePiramida() {
+        int size = Integer.parseInt(n); 
+
+        for (int i = 1; i <= size; i++) {
+            // Tipărim fiecare linie a piramidei
+            for (int j = 1; j <= size - i; j++) {
+                System.out.print(" "); 
+            }
+
+            for (int k = 1; k <= i; k++) {
+                System.out.print(i);  //
+            }
+
+            System.out.println();  //
+        }
+    }
+}
