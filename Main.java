dd38b6f496f8bb35d379df04695561afd2afbfe7
Denis-G25 <gabordenis1@gmail.com>
2025-03-27 21:32:49 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,13 +3,12 @@ public class Main {
     public static void main(String[] args) {
         
         Piramida piramida1 = new Piramida("4");
-        Piramida piramida2 = new Piramida("5");
+       
 
        
         System.out.println("Piramida 1:");
         piramida1.tiparestePiramida();
 
-        System.out.println("\nPiramida 2:");
-        piramida2.tiparestePiramida();
+        
     }
 }
